c8af0b3e62470cf35a8749bfc3655e2ba5dcb11e
osvaldo camacho <valdcam87@gmail.com>
2026-08-21 08:12:49 -0600

    added wrap config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -9,3 +9,15 @@ vim.opt.tabstop = 4 -- Espacios que representa un tabulador en pantalla
 vim.opt.shiftwidth = 4 -- Espacios aplicados en cada nivel de indentación (>)
 vim.opt.softtabstop = 4 -- Espacios que se agregan/borran al presionar Tab o Backspace
 vim.opt.expandtab = true -- Convierte automáticamente los pestañas en espacios
+
+-- Evita el scroll horizontal ajustando las líneas al ancho de pantalla
+vim.opt.wrap = true
+
+-- Mantiene la indentación original en las líneas envueltas
+vim.opt.breakindent = true
+
+-- No corta palabras a la mitad (rompe respetando los espacios)
+vim.opt.linebreak = true
+
+-- Opcional: agrega un pequeño indicador visual al inicio de la línea envuelta
+vim.opt.showbreak = "↪ "
